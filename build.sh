a7f71b1a9b993b888aa4430aebe2a4fe86f2b8b2
Travis CI <travis@travis-ci.org>
2019-12-19 14:31:55 +0800

    update,

--- build.sh
+++ build.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env sh
 
-sudo apt-get -y install cmake libboost-all-dev libcurl4-openssl-dev libnlopt-dev libwxgtk3.0-dev libcereal-dev libtbb-dev libudev-dev ninja-build
+# sudo apt autoremove
+# sudo apt-get -y install cmake libboost-all-dev libcurl4-openssl-dev libnlopt-dev libwxgtk3.0-dev libcereal-dev libtbb-dev libudev-dev ninja-build
 
-rm -rf build
-rm -rf CMakeFiles
-rm -rf cmake/CMakeFiles
-rm -rf cmake/CMakeCache.txt
+# rm -rf build
+# rm -rf CMakeFiles
+# rm -rf cmake/CMakeFiles
+# rm -rf cmake/CMakeCache.txt
 
-
+cd deps
 mkdir build
 cd build
-# cmake .. -DSLIC3R_WX_STABLE=1
-#   -DSLIC3R_STATIC=0 \
-# cmake ..\
-#   -DSLIC3R_FHS=1 \
-#   -DSLIC3R_WX_STABLE=1 \
-#   -DSLIC3R_PCH=0 \
-#   -DCMAKE_INSTALL_PREFIX=/usr \
-#   -DCMAKE_INSTALL_LIBDIR=lib
+cmake -G Ninja ..
+ninja
 
-cmake .. -DSLIC3R_WX_STABLE=1 -DSLIC3R_STATIC=0
-
-make -j
-
-# ls -l $TBBROOT
-# sudo apt-get install cmake libblkid-dev e2fslibs-dev libboost-all-dev libaudit-dev libtbb-dev libcurl3
+# cd ../..
+# mkdir build
+# cd build
+# cmake .. -G Ninja -DSLIC3R_STATIC=1 -DCMAKE_PREFIX_PATH="$PWD/../deps/build/destdir/usr/local"
+# ninja
