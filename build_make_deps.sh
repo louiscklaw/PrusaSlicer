d94dc97091f42353fb89e6b769fe7d66b82fdde7
louiscklaw <louiscklaw@gmail.com>
2019-12-20 23:58:21 +0800

    fallback to serial buidl,

--- build_make_deps.sh
+++ build_make_deps.sh
@@ -8,15 +8,15 @@ mkdir -p build
 cd build
 cmake ..
 
-make dep_libopenssl &
-make dep_wxwidgets &
-make dep_gtest &
-make dep_tbb &
-make dep_boost &
-make dep_cereal &
-make dep_nlopt &
-make dep_libcurl &
-make dep_qhull &
+make dep_wxwidgets
+make dep_libopenssl
+make dep_gtest
+make dep_tbb
+make dep_boost
+make dep_cereal
+make dep_nlopt
+make dep_libcurl
+make dep_qhull
 make dep_libigl
 
 wait
